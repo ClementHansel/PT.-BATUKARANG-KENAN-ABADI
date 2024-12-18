99c4e4a182c15040d80deb8c0a818c479ddc45b6
ClementHansel <greedybugz@gmail.com>
2024-12-18 22:26:51 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -315,24 +315,6 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-// Scroll to top Button
-// Select the button element
-const backToTopButton = document.getElementById("back-to-top");
-
-// Function to scroll to the top
-backToTopButton.addEventListener("click", () => {
-  window.scrollTo({ top: 0, behavior: "smooth" });
-});
-
-// Show or hide the button depending on scroll position
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 300) {
-    backToTopButton.classList.remove("hidden");
-  } else {
-    backToTopButton.classList.add("hidden");
-  }
-});
-
 // Whatsapp
 
 // Get references to the button and the chat popup
